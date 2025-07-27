a4e193962bef155ae5e1e3c8e8261ff792a8d758
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2025-07-27 22:54:17 +0530

    Update Main.java

--- LinkedList/Main.java
+++ LinkedList/Main.java
@@ -1,33 +1,61 @@
 
 public class Main{
      public static void main(String[] args) {
-        LL list = new LL();
+//        LL list = new LL();
 //      list.insertFirst(3);
 //      list.insertFirst(5);
 //      list.insertFirst(6);
 //      list.insertFirst(8);
 //
 //      list.display();
-        list.insertLast(5);
-        list.insertLast(6);
-        list.insertLast(7);
-        list.insertLast(8);
-        list.insertLast(9);
-        list.display();
+//        list.insertLast(5);
+//        list.insertLast(6);
+//        list.insertLast(7);
+//        list.insertLast(8);
+//        list.insertLast(9);
+//        list.display();
+//
+//        list.insert(15,2);
+//        list.display();
+//
+//        System.out.println(list.deleteFirst());
+//        list.display();
+//
+//        System.out.println(list.deleteLast());
+//        list.display();
+//
+//        System.out.println(list.delete(2));
+//        list.display();
+//
+//         System.out.println(list.find(15));
 
-        list.insert(15,2);
-        list.display();
+//         DLL list = new DLL();
+//         list.insertFirst(3);
+//         list.insertFirst(5);
+//         list.insertFirst(6);
+//         list.insertFirst(8);
+//
+//         list.display();
+//
+//         list.insertLast(65);
+//         list.insertLast(56);
+//         list.display();
 
-        System.out.println(list.deleteFirst());
-        list.display();
+         CLL clist = new CLL();
+         clist.insert(5);
+         clist.insert(25);
+         clist.insert(35);
+         clist.insert(55);
+         clist.display();
+         clist.delete(25);
+         clist.display();
 
-        System.out.println(list.deleteLast());
-        list.display();
 
-        System.out.println(list.delete(2));
-        list.display();
 
-         System.out.println(list.find(15));
+
+
+
+
 
 
 
