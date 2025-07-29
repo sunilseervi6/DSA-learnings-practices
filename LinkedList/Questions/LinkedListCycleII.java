b8ec15a45427d6bc071e8146bebfa3016e6793b6
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2025-07-29 23:47:57 +0530

    fast and slow pointers

--- LinkedList/Questions/LinkedListCycleII.java
+++ LinkedList/Questions/LinkedListCycleII.java
@@ -30,3 +30,56 @@ public class Solution {
     }
     
 }
+
+/*
+
+public class Solution {
+    public int cycleLength( ListNode head){
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                int count =0;
+                ListNode temp = slow;
+                do{
+                    temp=temp.next;
+                    count++;
+                }while(temp != slow);
+                return count;
+            }
+        }
+        return 0;
+    }
+    public ListNode detectCycle(ListNode head){
+        int length=0;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next !=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                length = cycleLength(slow);
+                break;
+            }
+        }
+        if(length ==0){
+            return null;
+        }
+        //find start
+        ListNode f = head;
+        ListNode s = head;
+        while(length > 0){
+            s=s.next;
+            length--;
+        }
+        //keep moving both forward
+        while(f!=s){
+            f=f.next;
+            s=s.next;
+        }
+        return s;
+    }
+}
+*/
