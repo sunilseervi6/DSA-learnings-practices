7f80b071c2f857086ea41f48edf9716a7a6337be
Sunil Seervi <115252067+sunilseervi6@users.noreply.github.com>
2025-08-05 23:20:46 +0530

    Create CombinationSum.java

---
+++ Recursion/CombinationSum.java
@@ -0,0 +1,23 @@
+//lc 39
+
+class Solution {
+    public void find(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds){
+        if(ind == arr.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        if(arr[ind] <= target){
+           ds.add(arr[ind]);
+           find(ind, arr, target-arr[ind], ans, ds);
+           ds.remove(ds.size() - 1); 
+        }
+        find(ind+1, arr, target, ans, ds);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        find(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+}
